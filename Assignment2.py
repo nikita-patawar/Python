e5b3c480d644e82402e3be20bd5205fe9c5746cf
Nikita <patawarnikita@gmail.com>
2025-12-28 14:40:12 +0530

    Initial commit

---
+++ Assignment2.py
@@ -0,0 +1,17 @@
+'''
+Write a program to display value,type,memory address
+
+'''
+
+a = 10
+b = 10
+
+print(a)
+print(type(a))
+print(id(a))
+
+'''
+What is difference between a = 10 & b = 10
+
+'''
+print(id(a), id(b))
